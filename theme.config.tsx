b30f974295522cb0f5525cad96ec9d4b8d6f86be
Rithy <rithynathan@gmail.com>
2024-11-23 18:27:59 +0100

    Daily Commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <img src="https://starblast.io/static/img/icon64.png" alt="Logo" width="32" height="32"/>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
